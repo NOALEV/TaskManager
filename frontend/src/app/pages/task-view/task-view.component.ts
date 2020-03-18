73451d44428ec3955c500a1cf6918533cd568aa9
noaal <noaal@LAPTOP-CSF409U0>
2020-03-18 16:59:38 +0200

    Users&SignIn&Signup

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from 'src/app/task.service';
-import { TmplAstElement } from '@angular/compiler';
-import { ActivatedRoute, Params } from '@angular/router';
-import { List } from 'src/app/models/list.model';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Task } from 'src/app/models/task.model';
+import { List } from 'src/app/models/list.model';
 
 @Component({
   selector: 'app-task-view',
@@ -11,34 +10,55 @@ import { Task } from 'src/app/models/task.model';
   styleUrls: ['./task-view.component.scss']
 })
 export class TaskViewComponent implements OnInit {
-lists:List[];
-tasks:Task[];
-  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
+
+  lists: List[];
+  tasks: Task[];
+
+  selectedListId: string;
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
+        if (params.listId) {
+          this.selectedListId = params.listId;
+          this.taskService.getTasks(params.listId).subscribe((tasks: Task[]) => {
+            this.tasks = tasks;
+          })
+        } else {
+          this.tasks = undefined;
+        }
+      }
+    )
+
+    this.taskService.getLists().subscribe((lists: List[]) => {
+      this.lists = lists;
+    })
     
-        this.taskService.getTasks(params.listId).subscribe((tasks: any[]) => {
-          this.tasks=tasks;
-        })
   }
-    )
-    this.taskService.getList().subscribe((lists:any[])=>{
-    this.lists=lists;
-    }
-    )
+
+  onTaskClick(task: Task) {
+    // we want to set the task to completed
+    this.taskService.complete(task).subscribe(() => {
+      // the task has been set to completed successfully
+      console.log("Completed successully!");
+      task.completed = !task.completed;
+    })
+  }
+
+  onDeleteListClick() {
+    this.taskService.deleteList(this.selectedListId).subscribe((res: any) => {
+      this.router.navigate(['/lists']);
+      console.log(res);
+    })
+  }
+
+  onDeleteTaskClick(id: string) {
+    this.taskService.deleteTask(this.selectedListId, id).subscribe((res: any) => {
+      this.tasks = this.tasks.filter(val => val._id !== id);
+      console.log(res);
+    })
+  }
+
 }
-
-onTaskClick(task: Task) {
-  // we want to set the task to completed
-  this.taskService.complete(task).subscribe(() => {
-    // the task has been set to completed successfully
-    console.log("Completed successully!");
-    task.completed = !task.completed;
-  })
-}
-
-}
-
-
